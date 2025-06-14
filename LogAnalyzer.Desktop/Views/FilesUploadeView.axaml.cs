2eb8a0a11a185412a67931b493c2e759053756e7
maryshev_av <maryshev_av@rtsoft.ru>
2025-06-14 09:21:28 +0000

    Сделал заготовку под Ctrl (Command) + V файлов с логами

--- LogAnalyzer.Desktop/Views/FilesUploadeView.axaml.cs
+++ LogAnalyzer.Desktop/Views/FilesUploadeView.axaml.cs
@@ -1,13 +1,77 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace LogAnalyzer.Desktop.Views;
 
 public partial class FilesUploadeView : UserControl
 {
+    private readonly string _dragAndDropElementName = "DragAndDropElement";
+    private readonly string _overedDragAndDropBorderBrushName = "OveredDragAndDropBorderBrush";
+
+    private IBrush? _originDragAndDropBorderBrush;
+    private IBrush _overedDragAndDropBorderBrush;
+    
     public FilesUploadeView()
     {
         InitializeComponent();
+
+        InitializeDragAndDrop();
     }
+
+    private void InitializeDragAndDrop()
+    {
+        var dragAndDrop = this.Find<Border>(_dragAndDropElementName);
+
+        _originDragAndDropBorderBrush = dragAndDrop.BorderBrush;
+
+        if (Application.Current.TryFindResource(
+            _overedDragAndDropBorderBrushName,
+            out var overedDragAndDropBorderBrush
+        ))
+        {
+            _overedDragAndDropBorderBrush = (SolidColorBrush)overedDragAndDropBorderBrush;
+        };
+        
+        dragAndDrop.AddHandler(DragDrop.DragOverEvent, OnDragOverBorder);
+        dragAndDrop.AddHandler(DragDrop.DragLeaveEvent, OnDragLeaveBorder);
+        dragAndDrop.AddHandler(DragDrop.DropEvent, OnDropBorder);
+    }
+    
+    private void OnDragOverBorder(object? sender, DragEventArgs e)
+    {
+        if (e.Data.Contains(DataFormats.FileNames))
+        {
+            // Изменяем цвет границы при наведении.
+            ((Border)sender).BorderBrush = _overedDragAndDropBorderBrush;
+            
+            e.DragEffects = DragDropEffects.Copy;
+        }
+        else
+        {
+            e.DragEffects = DragDropEffects.None;
+        }
+    }
+
+    private void OnDragLeaveBorder(object? sender, DragEventArgs e)
+    {
+        // Возвращаем исходную заливку Border при уходе курсора
+        ((Border)sender).BorderBrush = _originDragAndDropBorderBrush;
+    }
+
+    private void OnDropBorder(object? sender, DragEventArgs e)
+    {
+        // После дропа возвращаем исходную заливку и обрабатываем файлы
+        ((Border)sender).BorderBrush = _originDragAndDropBorderBrush;
+        
+        if (e.Data.Contains(DataFormats.FileNames))
+        {
+            var files = e.Data.GetFileNames();
+            
+            // Обработка файлов.
+        }
+    }
+
 }
--- LogAnalyzer.Desktop/Views/MainWindow.axaml.cs
+++ LogAnalyzer.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Platform.Storage;
 
 namespace LogAnalyzer.Desktop.Views;
 
@@ -7,5 +10,30 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        this.KeyDown += OnKeyDown;
     }
+    
+    private bool IsCalledPasteCommand(KeyEventArgs e) 
+        => e.Key == Key.V &&
+        (
+            (e.KeyModifiers & KeyModifiers.Control) == KeyModifiers.Control
+            || (e.KeyModifiers & KeyModifiers.Meta) == KeyModifiers.Meta
+        );
+    
+    private async void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (IsCalledPasteCommand(e))
+        {
+            var clipboard = TopLevel.GetTopLevel(this).Clipboard;
+            
+            var data = await clipboard.GetDataAsync(DataFormats.Files);
+
+            if (data != null && data is IEnumerable<IStorageItem> files)
+            {
+                ;
+            }
+        }
+    }
+
 }
